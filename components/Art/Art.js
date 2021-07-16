00c5bbd9e47f2f69a6057d98be1d271101e868e3
gelicamarie <turla_angelica@yahoo.com>
2021-07-16 00:42:34 -0400

    fix build issues

--- components/Art/Art.js
+++ components/Art/Art.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Container, Artwork, ArtInfo, Title, Price, Owner } from "./Art.styled";
-import blurImage from "../../../lib/blur-image";
+import blurImage from "../../lib/blur-image";
 
 const Art = ({ title, url, owner, price, width, height, to }) => {
   return (
     <Link href={to}>
       <Artwork>
-        <Image src={url} alt={title} width={width} height={height} placeholder="blur" blurDataURL={blurImage(width, height)}/>
+        <Image
+          src={url}
+          alt={title}
+          width={width}
+          height={height}
+          placeholder="blur"
+          blurDataURL={blurImage(width, height)}
+        />
         <ArtInfo>
           <Container>
             <Title>{title}</Title>
--- pages/artwork/[id].js
+++ pages/artwork/[id].js
@@ -1,20 +1,72 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../../components/Navbar/Navbar";
-import {
-  Line,
-  Container,
-  Info,
-  Section,
-  Title,
-  User,
-  Menu,
-  MenuItem,
-  Description,
-  Price,
-  BuyBtn,
-} from "./artwork.styled";
-import blurImage from "../../../lib/blur-image";
+import blurImage from "../../lib/blur-image";
+import styled from "styled-components";
+
+const Line = styled.hr`
+  margin: 0 0 1rem;
+  border-top: 0.18rem solid black;
+`;
+const Container = styled.div`
+  display: flex;
+  height: 80vh;
+  align-items: center;
+  margin: 0;
+`;
+
+const Info = styled.div`
+  width: 43%;
+  margin: 0 3rem;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const Title = styled.h1`
+  margin: 0 0 0.5rem;
+`;
+const User = styled.h3`
+  color: var(--color-theme-orange-1);
+  font-weight: 500;
+  margin: 0;
+  font-size: 1.1rem;
+`;
+const Menu = styled.ul`
+  display: flex;
+  list-style-type: none;
+  margin: 1rem 0 0.2rem;
+  padding: 0;
+  width: 100%;
+`;
+
+const MenuItem = styled.li`
+  margin: 0.5rem 0 0;
+  font-weight: 500;
+  font-size: 1.1rem;
+`;
+
+const Description = styled.div`
+  height: 12rem;
+  font-size: 1.15rem;
+`;
+
+const Price = styled.h3`
+  margin: 4rem 0 0 0.8rem;
+  letter-spacing: 0.2rem;
+  font-size: 1.3rem;
+`;
+
+const BuyBtn = styled.button`
+  font-size: 1.2rem;
+  background-color: #606c38;
+  color: var(--color-theme-bg);
+  border: none;
+  border-radius: 50px;
+  padding: 1rem 4rem;
+`;
 
 const Item = ({ nft }) => {
   return (
